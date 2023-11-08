e052e8baf3a87975884eca5df17710ae11dda070
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2023-11-08 23:11:14 +0300

    add dict with languages, recive count

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import requests
 
+languages = ["Python", "Java", "Javascript"]
+languages_vatations = {}
 
-params = {
-    "text": "программист",
-    "date_from": "2023-10-15"
-     }
-response = requests.get('https://api.hh.ru/vacancies/',  params=params)
-print(response.raise_for_status())
-print(response.status_code)
-print(response.text)
-
-languages = {}
+for language in languages:
+    params = {
+        "text": language,
+        "date_from": "2023-10-15"
+        }
+    response = requests.get('https://api.hh.ru/vacancies/',  params=params)
+    response.raise_for_status()
+    print(response.status_code)
+    language_info = response.json()
+    count = language_info["found"]
+    languages_vatations[language] = count
+print(languages_vatations)
