11591629f47efb2ff8e79b2dced7cbf0777a0461
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2023-11-09 21:14:14 +0300

    recive selary

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import requests
+# находим колличество вакансий на определенном языке
+# languages = ["Python", "Java", "Javascript", "C", "C#", "F#", "Ruby", "Go", "Golang"]
+# languages_vacations = {}
 
-languages = ["Python", "Java", "Javascript", "C", "C#", "F#"]
-languages_vatations = {}
-
-for language in languages:
-    params = {
-        "text": language,
-        "date_from": "2023-10-15"
-        }
-    response = requests.get('https://api.hh.ru/vacancies/',  params=params)
-    response.raise_for_status()
-    print(response.status_code)
-    language_info = response.json()
-    count = language_info["found"]
-    languages_vatations[language] = count
-print(languages_vatations)
+# for language in languages:
+#     params = {
+#         "text": language,
+#         "date_from": "2023-10-15"
+#         }
+#     response = requests.get('https://api.hh.ru/vacancies/',  params=params)
+#     response.raise_for_status()
+#     language_info = response.json()
+#     count = language_info["found"]
+#     languages_vacations[language] = count
+# print(languages_vacations)
+# выводим зарплату из вакансий
+language = "Python"
+params = {
+    "text": language,
+    "date_from": "2023-10-15"  
+    }
+response = requests.get('https://api.hh.ru/vacancies/',  params=params)
+response.raise_for_status()
+language_info = response.json()
+vacations = language_info["items"]
+for vacation in vacations:
+    print(vacation["salary"])
