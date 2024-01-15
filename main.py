75da9bfd693023f738ac1d20b76cfcc75798425d
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2024-01-15 20:49:39 +0300

    add git ignore

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import os
+from dotenv import load_dotenv, find_dotenv
 # находим колличество вакансий на определенном языке
 # languages = ["Python", "Java", "Javascript", "C", "C#", "F#", "Ruby", "Go", "Golang"]
 # languages_vacations = {}
@@ -45,46 +47,62 @@ import requests
 # находим количество вакансий по языка, среднюю запрлату и кол вакансий из которых считали среднюю 
 
 
-def predict_rub_salary(salary):
-    if str(salary["currency"]) == "RUR":
-        if salary["from"] == None:
-            midlle = int(salary["to"])*0.8
-        elif salary["to"] == None:
-            midlle = int(salary["from"])*1.2
-        else:
-            midlle = (int(salary["from"]) + int(salary["to"]))/2
-        return midlle
+# def predict_rub_salary(salary):
+#     if str(salary["currency"]) == "RUR":
+#         if salary["from"] == None:
+#             midlle = int(salary["to"])*0.8
+#         elif salary["to"] == None:
+#             midlle = int(salary["from"])*1.2
+#         else:
+#             midlle = (int(salary["from"]) + int(salary["to"]))/2
+#         return midlle
 
 
-languages = ["Python", "Java", "Javascript", "C", "C#", "F#", "Ruby", "Go", "Golang"]
-languages_vacations = {}
+# languages = ["Python", "Java", "Javascript", "C", "C#", "F#", "Ruby", "Go", "Golang"]
+# languages_vacations = {}
 
-for language in languages:
-    page = 0
-    pages_number = 1
-    while page < 2:
-        params = {
-            "text": language,
-            }
-        response = requests.get('https://api.hh.ru/vacancies/',  params=params)
-        response.raise_for_status()
-        language_info = response.json()
-        pages_number = language_info['pages']
-        page += 1
-        count = language_info["found"]
-        languages_vacations[language] = {"vacancies_found": count}
-        vacations = language_info["items"]
-        mid_summ = 0
-        vacancies_processed = 0
-        for vacation in vacations:
-            salary = vacation["salary"]
-            if salary:
-                mid = predict_rub_salary(salary)
-                if mid:
-                    mid_summ += mid
-                    vacancies_processed += 1
-        languages_vacations[language]["vacancies_processed"] = vacancies_processed
-        average_salary = mid_summ/vacancies_processed
-        languages_vacations[language]["average_salary"] = average_salary
-for language in languages_vacations.items():
-    print(language[0], language[1])
+# for language in languages:
+#     page = 0
+#     pages_number = 1
+#     while page < 2:
+#         params = {
+#             "text": language,
+#             }
+#         response = requests.get('https://api.hh.ru/vacancies/',  params=params)
+#         response.raise_for_status()
+#         language_info = response.json()
+#         pages_number = language_info['pages']
+#         page += 1
+#         count = language_info["found"]
+#         languages_vacations[language] = {"vacancies_found": count}
+#         vacations = language_info["items"]
+#         mid_summ = 0
+#         vacancies_processed = 0
+#         for vacation in vacations:
+#             salary = vacation["salary"]
+#             if salary:
+#                 mid = predict_rub_salary(salary)
+#                 if mid:
+#                     mid_summ += mid
+#                     vacancies_processed += 1
+#         languages_vacations[language]["vacancies_processed"] = vacancies_processed
+#         average_salary = mid_summ/vacancies_processed
+#         languages_vacations[language]["average_salary"] = average_salary
+# for language in languages_vacations.items():
+#     print(language[0], language[1])
+
+#Запрос к сайту СуперДжоб
+load_dotenv(find_dotenv())
+#params = {"X-Api-App-Id": os.getenv("superjob_key")}
+#params = {"Authorization": f'Bearer {os.getenv("superjob_key")}'}
+#params = {"Authorization": 'X-Api-App-Id'}
+#params = {"Authorization": os.getenv("superjob_key")}
+#params = {"Authorization": f'Bearer X-Api-App-Id'}
+headers = {"X-Api-App-Id": os.getenv("superjob_key")}
+
+response = requests.get('https://api.superjob.ru/2.0/vacancies/',  headers=headers)
+response.raise_for_status()
+language_info = response.json()
+language = language_info["objects"]
+for i in language:
+    print(i["profession"])
