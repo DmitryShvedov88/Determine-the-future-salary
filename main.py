7433d4ad2d2046f7e5216df5f030b0715c9a17c4
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2024-01-26 20:38:55 +0300

    fix some bags

--- main.py
+++ main.py
@@ -3,50 +3,6 @@ import os
 from dotenv import load_dotenv, find_dotenv
 from terminaltables import AsciiTable
 load_dotenv(find_dotenv())
-#находим колличество вакансий на определенном языке
-# languages = ["Python", "Java", "Javascript", "C", "C#", "F#", "Ruby", "Go", "Golang"]
-# languages_vacations = {}
-
-# for language in languages:
-#     params = {
-#         "text": language,
-#         "date_from": "2023-10-15"
-#         }
-#     response = requests.get('https://api.hh.ru/vacancies/',  params=params)
-#     response.raise_for_status()
-#     language_info = response.json()
-#     count = language_info["found"]
-#     languages_vacations[language] = count
-# print(languages_vacations)
-
-# выводим зарплату из вакансий
-# def predict_rub_salary(salary):
-#     if salary == None:
-#         print("None")
-#     else:
-#         if str(salary["currency"]) == "RUR":
-#             if salary["from"] == None:
-#                 mid = int(salary["to"])*0.8
-#                 print(mid)
-#             elif salary["to"] == None:
-#                 mid = int(salary["from"])*1.2
-#                 print(mid)       
-#             else:
-#                 mid = (int(salary["from"]) + int(salary["to"]))/2
-#                 print(mid)
-
-# language = "Python"
-# params = {
-#     "text": language,
-#     "date_from": "2023-10-15"
-# response = requests.get('https://api.hh.ru/vacancies/',  params=params)
-# response.raise_for_status()
-# language_info = response.json()
-# vacations = language_info["items"]
-# for vacation in vacations:
-#     salary = vacation["salary"]
-#     predict_rub_salary(salary)
-#
 
 
 def print_table(title, languages_vacations):
@@ -62,16 +18,19 @@ def print_table(title, languages_vacations):
 
 
 # находим количество вакансий по языка, среднюю запрлату и кол вакансий из которых считали среднюю
+
+
 def predict_rub_salary(salary_from, salary_to):
-        if salary_from == None:
-            midlle = int(salary_to)*0.8
-        elif salary_to == None:
-            midlle = int(salary_from)*1.2
-        else:
-            midlle = (int(salary_from) + int(salary_to))/2
-        if midlle == 0:
-            return None
-        return midlle
+    if salary_from == None:
+        midlle = int(salary_to)*0.8
+    elif salary_to == None:
+        midlle = int(salary_from)*1.2
+    else:
+        midlle = (int(salary_from) + int(salary_to))/2
+    if midlle == 0:
+        return None
+    return midlle
+# Запрос к сайту СуперДжоб
 
 
 languages = ["Python", "Си", "SQL"]
@@ -111,12 +70,14 @@ title = "HeadHunter Moscow"
 
 print_table(title, languages_vacations)
 
-#Запрос к сайту СуперДжоб
+# Запрос к сайту СуперДжоб
+
 
 def predict_rub_salary_sj(vacation):
     payment_from, payment_to = vacation["payment_from"], vacation["payment_to"]
     mid = predict_rub_salary(payment_from, payment_to)
     return mid
+
 
 languages = ["Python", "Си", "SQL"]
 languages_vacations = {}
